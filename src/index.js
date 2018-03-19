c80bcab1181573e19393d2c03867f3e249cd1734
Jacob Vannucci <vannucci.j@gmail.com>
2018-03-19 09:47:37 -0500

    This is my first commit for the TNLand website

--- src/index.js
+++
@@ -1,6 +0,0 @@
-/** 
- * Main entrypoint for the program.  Reads the conf, sets up the modules, runs the server.
- */
-'use strict';
-let app    = require('./app')();
-let server = require('./server')(app, 3000);
